0002f93125a4d0a4839602fac108762e03d1a1c8
syn-127 <c_sampere@hotmail.com>
2021-01-06 00:23:27 +0000

    First Commit
    initial commit with predone files

---
+++ admin/config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+/* URL PROJECT */
+
+define ('SITE_URL', 'YOUR_URL_HERE');
+
+/* DATABASE CONFIGURATION */
+
+$database = array(
+'host' => 'DATABASE_HOST_HERE',
+'db' => 'DATABASE_NAME_HERE',
+'user' => 'DATABASE_USER_HERE',
+'pass' => 'DATABASE_PASSWORD_HERE'
+);
+
+$email_config = array(
+'email_address' => 'EMAIL_ADDRESS_HERE',
+'email_password' => 'PASSWORD_HERE',
+'email_subject' => 'EMAIL_SUBJECT_HERE',
+'email_name' => 'EMAIL_NAME_HERE',
+'smtp_host' => 'EMAIL_HOST_HERE',
+'smtp_port' => 'EMAIL_PORT_HERE',
+'smtp_encrypt' => 'tls'
+);
+
+$items_config = array(
+    
+    'items_per_page' => '8',
+    'images_folder' => 'images/'
+);
+
+
+?>
